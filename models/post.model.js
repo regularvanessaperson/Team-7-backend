b5a9600471f0cae1a45b99e6ca7b498f1ce6e154
billymoroney <YOUR-EMAIL-ADDRESS>
2020-12-23 18:50:38 -0600

    created post model

---
+++ models/post.model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const post = mongoose.model(
+    "Post",
+    new mongoose.Schema({
+        creator: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
+        // creatorThumbnail: [],
+        timestamp: Date,
+        body: String,
+        favorites: Number,
+        favoritedBy: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
+        reposts: Number,
+        repostedBy: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
+        replies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Post'}],
+        // topics: []
+        hashtags: [],
+        isRepost: Boolean,
+        isReply: Boolean,
+        parentPost: [{type: mongoose.Schema.Types.ObjectId, ref: 'Post'}],
+        // media: []
+    })
+)
+
+module.exports = Post
