136ba96c7c022e335b8741ef152b9bdb437d147e
billymoroney <YOUR-EMAIL-ADDRESS>
2020-12-23 18:23:55 -0600

    updated user model

--- models/user.model.js
+++ models/user.model.js
@@ -6,6 +6,16 @@ const User = mongoose.model(
         username: String,
         email: String,
         password: String,
+        followed: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
+        followers: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
+        favoritePosts: [{type: mongoose.Schema.Types.ObjectId, ref: 'Posts'}],
+        reposts: [{type: mongoose.Schema.Types.ObjectId, ref: 'Posts'}],
+        posts: [{type: mongoose.Schema.Types.ObjectId, ref: 'Posts'}],
+        location: String,
+        birthday: Date,
+        dateRegistered: Date,
+        // profilePic: [],
+        // bannerPic: [],
         roles: [
             {
                 type: mongoose.Schema.Types.ObjectId,
