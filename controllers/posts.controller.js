d3fb8c20e0b25e630fbf271d084b01601f8c36e8
regularvanessaperson <69284928+regularvanessaperson@users.noreply.github.com>
2020-12-26 14:50:20 -0600

    completed routes to onePost and allPosts tested in postman successfully (#3)

--- controllers/posts.controller.js
+++ controllers/posts.controller.js
@@ -70,3 +70,28 @@ exports.deletePost = (req,res) => {
         else res.send(data)
     })
 }
+
+//route to display all posts "/api/posts/feed" 
+exports.allPosts = (req, res) => {
+    Post.find()
+    .then((data)=> {
+        res.send(data)
+    })
+    .catch((err) => {
+        res.status(500).send({
+            message: err || "Some error occured while retreiving tutorials"
+        })
+    })
+}
+
+//route to display one post only "/api/posts/:id"
+exports.onePost = (req, res) => {
+    const id = req.params.idx
+    console.log(id)
+    Post.find({_id: id})
+    .then((post)=>{
+        if(!post)
+        return res.status(400).send({message: "Cannot find this post"})
+        else res.send(post)
+    })
+}
--- routes/posts.routes.js
+++ routes/posts.routes.js
@@ -18,16 +18,16 @@ app.post("/api/posts/post", controller.makePost)
 app.put("/api/posts/post", controller.editPost)
 //Delete a post
 app.delete("/api/posts/post", controller.deletePost)
-// //Retreive all of the posts to display on home
-// app.get("/api/posts/feed", controller.allPosts)
-// //Retreive a single post 
-// app.get("/api/posts/:id", controller.onePost)
 // //Post repost/retweet - should get original and display as retweet
 // app.post("/api/posts/retweet", controller.retweetPost)
 // //Reply to post
 // app.post("/api/posts/reply", controller.replyToPost)
 // //View all posts from users as user is following
 // app.get("/api/posts/feed/follow", controller.userFollowing)
+//Retreive all of the posts to display on home
+app.get("/api/posts/feed", controller.allPosts)
+// //Retreive a single post 
+app.get("/api/posts/:idx", controller.onePost)
 
 
 }
