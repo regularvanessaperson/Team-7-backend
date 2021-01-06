af6b8364adbf4fab704837aae349d40f2bb5985e
billymoroney <YOUR-EMAIL-ADDRESS>
2021-01-05 20:08:27 -0600

    tried to get multer set up to upload pics

---
+++ routes/image.routes.js
@@ -0,0 +1,34 @@
+const multer = require('multer')
+const upload = multer({dest: 'uploads/'})
+
+
+const controller = require('../controllers/auth.controller');
+// const { checkDuplicateUsernameOrEmail } = require('../middlewares/verifySignup');
+
+module.exports = function(app) {
+    
+    app.use((req,res, next)=> {
+        //set header and allow use of x access token (we will use this to pass our token)
+        res.header(
+            "Access-Control-Allow-Headers",
+            "x-access-token, Origin, Content-type, Accept"
+        );
+        next();
+    })
+    
+    app.post('/api/photo', upload.single('avatar'), (req, res) => {
+        let newImage = new Image()
+        newImage.img.data = fs.readFileSync(req.file)
+        newImage.img.contentType = 'image/png'
+        newImage.save((err) => {
+        console.log('Image Saved!', newImage._id)
+    })
+
+    User.findById(req.body.userId, (err, user) => {
+        user.profilePic.push(newImage._id)
+        user.save((err) => {
+            console.log('Image referenced by user!')
+        })
+    })
+    })
+}
